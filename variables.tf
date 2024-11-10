7395461c3056f96d5ba8c1aa56f0004844cd494f
praveensri453 <praveensri92@gmail.com>
2024-11-10 19:54:36 +0530

    module

---
+++ variables.tf
@@ -0,0 +1,24 @@
+
+variable "project_name" {
+
+}
+
+variable "environment" {
+
+}
+
+variable "sg_name" {
+
+}
+
+variable "common_tags" {
+    default = {}
+}
+
+variable "sg_tags" {
+    default = {}
+}
+
+variable "vpc_id" {
+    
+}
